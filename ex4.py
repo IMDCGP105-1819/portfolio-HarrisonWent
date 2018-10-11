bd13d9cb847a717a133bed50f104f46e71c03948
Harrison Went <wentharrison@gmail.com>
2018-10-11 11:31:53 +0100

    Added week 1 pyhton exercises

---
+++ ex4.py
@@ -0,0 +1,17 @@
+my_name = 'Harrison Went'
+my_age = 19 # maybe
+my_height = 67 # inches
+my_weight = 140 # pounds
+my_eyes = 'Green'
+my_hair = 'Ginger'
+is_heavy = my_weight > 3000
+
+print(f"Let's talk about {my_name}.")
+# swap in your preferred pronoun.
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
