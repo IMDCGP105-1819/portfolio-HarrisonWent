d7551b205a289db74420ef29c6c5cf3bea44dac5
Harrison Went <wentharrison@gmail.com>
2018-10-18 11:02:05 +0100

    Updated week 2 ex7 and added Week 3 ex8

--- ex8.py
+++ ex8.py
@@ -1,4 +1,6 @@
 Annual_Salary = float(input("Please enter your annual salary: "))
+semi_annual_raise  = float(input("Please enter your semi annual raise: "))
+
 Portion_Saved = float(input("Please enter your percentage of your salary to be saved : "))
 Total_House_Cost = float(input("Please enter the total cost of the house: "))
 
@@ -11,6 +13,8 @@ while Current_Savings<Total_House_Cost:
     Current_Savings+=(Annual_Salary/12*Portion_Saved)
     Current_Savings += Current_Savings * (r / 12)
     Months_To_Save+=1
+    if 6%Months_To_Save==0:
+        Annual_Salary= Annual_Salary+((Annual_Salary/2)*semi_annual_raise)
 
 print("Number of months: ", Months_To_Save)
 
