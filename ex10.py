b4aa7ee4855cdd0e93316279b414713274413a1d
Harrison Went <wentharrison@gmail.com>
2018-10-18 11:59:38 +0100

    Updated week 2 ex7 and added Week 3 ex8

--- ex10.py
+++ ex10.py
@@ -1,6 +1,6 @@
 High = float(input("Please enter highest number: "))
 Low = float(input("Please enter lowest number: "))
-for Number in range (1,100):
+for Number in range (10,100):
     if Number % 3 == 0 & Number % 5:
         print("FizzBuzz")
     elif Number % 5 == 0:
