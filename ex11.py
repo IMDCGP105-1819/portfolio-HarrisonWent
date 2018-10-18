9fdb1dfa9f2f413fd1902befc171a87f9fbd9a0d
Harrison Went <wentharrison@gmail.com>
2018-10-18 11:58:53 +0100

    Week 4 ex11 improvements

--- ex11.py
+++ ex11.py
@@ -3,11 +3,20 @@ from random import randint
 High = float(input("Please enter highest number: "))
 Low = float(input("Please enter lowest number: "))
 
-def RandomNumberGuess(hi, lo):
+def RandomNumberGuess(hi,lo):
+
     GeneratedNumber = randint(lo,hi)
-    Guess = float(input("Guess the number: "))
-    if Guess == GeneratedNumber:
-        print("You guessed correctly!")
+    NotCorrect = True
+
+    while NotCorrect == True:
+        guess = float(input("Guess the number: "))
+        if guess == GeneratedNumber:
+            print("You guessed correctly!")
+            NotCorrect = False
+        elif guess<GeneratedNumber:
+            print("you guessed too low!")
+        else:
+            print("you guessed too high!")
     return
 
 RandomNumberGuess(High,Low)
