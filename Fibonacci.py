11d9e3b63ed164780c0a16dd6ea0864a545d5c60
Harrison Went <wentharrison@gmail.com>
2018-11-01 11:48:05 +0000

    Finished fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,20 +1,20 @@
 Fib_Value = int(input("Please enter the nth fibonacci: "))
-Answer = 0
 Second_Last_Number = 0
-Last_Number = 1
+Last_Number = 0
 
 
-def fib(fib_value, second_last_number, last_number, answer):
+def fib(fib_value, second_last_number, last_number):
 
-    answer = Last_Number + second_last_number
+    answer = last_number + second_last_number
 
-    second_last_number = Last_Number
-    last_number = Answer
-
-    if fib_value == 0:
+    if fib_value == 2:
         return answer
     else:
-        fib(fib_value-1, second_last_number, last_number, answer)
+        second_last_number = last_number
+        last_number = answer
+        if last_number == 0:
+            last_number = 1
+        return fib(fib_value-1, second_last_number, last_number)
 
 
-print(fib(Fib_Value, Second_Last_Number, Last_Number, Answer))
+print(fib(Fib_Value, Second_Last_Number, Last_Number))
